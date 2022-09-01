0f2172eca9ad48c1de35f57854ab69226272add1
zolark1234 <carlos13zuleta@gmail.com>
2022-09-01 11:31:15 -0500

    pruebas en el main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,27 @@
 package main;
+import java.util.*;
 
 import model.Tablero;
 
 public class Main {
+
+    public static Scanner sc=new Scanner(System.in);
+    public static Tablero tablero = new Tablero();
     public static void main(String[] args) {
-        Tablero tablero = new Tablero();
-        System.out.println(tablero.toStringT());
-        int c= 0;
+        //System.out.println(tablero.toStringT());
+        while (true){
+            System.out.println(tablero.toStringT());
+            sc.nextLine();
+            System.out.println("---------");
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            sc.nextLine();
+            System.out.println("------------");
+            String h=sc.nextLine();
+
+
+            tablero.cambiar(x-1,y-1, h);
+            //System.out.println(tablero.simular());
+        }
     }
 }
