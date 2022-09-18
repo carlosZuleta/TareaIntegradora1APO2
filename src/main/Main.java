94b12248b0017bcd7522ce381488ec75016ea288
zolark1234 <carlos13zuleta@gmail.com>
2022-09-17 20:56:11 -0500

    entrega final incompleta

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,8 +8,64 @@ public class Main {
     public static Scanner sc=new Scanner(System.in);
     public static Tablero tablero = new Tablero();
     public static void main(String[] args) {
+        boolean salir = false;
+        int opt;
+        String nick = "";
+
+        while(!salir){
+            System.out.println("/--------------------------------/");
+            System.out.println("1. Nueva partida");
+            System.out.println("2. Ver puntaje");
+            System.out.println("3. Salir");
+            System.out.println("/--------------------------------/");
+
+            System.out.println("-----eliga una opcion----");
+            opt = sc.nextInt();
+
+            switch (opt){
+                case 1:
+                    System.out.pintln("Digite su nickName de jugador:");
+                    nick = sc.nextLine();
+
+                    System.out.println("Bienvenido "+nick);
+
+                    System.out.println("/--------------------/");
+                    System.out.println(tablero.toStringT());
+                    menu2();
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+
+            }
+            public void menu2(){
+                boolean salir2 = false;
+                int opt2;
+
+                while(!salir2){
+                    System.out.println("/--------------------------------/");
+                    System.out.println("1. poner tuberia");
+                    System.out.println("2. simular");
+                    System.out.println("3. Salir");
+                    System.out.println("/--------------------------------/");
+
+                    System.out.println("-----eliga una opcion----");
+                    opt2 = sc.nextInt();
+                    switch (opt2){
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                    }
+                }
+            }
+        }
         //System.out.println(tablero.toStringT());
-        while (true){
+        /*while (true){
             System.out.println(tablero.toStringT());
             sc.nextLine();
             System.out.println("---------");
@@ -22,6 +78,11 @@ public class Main {
 
             tablero.cambiar(x-1,y-1, h);
             //System.out.println(tablero.simular());
-        }
+        }*/
+
+
+
+
     }
+
 }
